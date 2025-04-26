c256d7b6870e4831d918fee8e9198e78fe348074
MateusFGC <165953733+MateusFGC@users.noreply.github.com>
2025-04-26 13:00:10 -0300

    Inserir a atualização dos dados
    Update.
    Buscar os dados em um objeto para ser usados nas aplicações necessárias.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, asdict
 from pathlib import Path
 from typing import Literal
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 
 
 @dataclass
@@ -12,6 +12,10 @@ class Todo:
 
     def as_dict(self):
         return asdict(self)
+    
+    @classmethod
+    def from_dict(cls, data):
+        return Todo(**data)
 
 #Dados do BD
 t1 = Todo('buy coffee', 'Mateus', 'todo')    
@@ -21,16 +25,30 @@ t2 = Todo('Walk the dog', 'Mateus')
 db_path = Path(__file__).parent / 'db.json'
 db = TinyDB(db_path, indent=4)
 
-#Limpa os dados repetidos
+#Limpa a base de dados repetidas
 db.truncate()
 
-#2 formas diferentes de inserir o dados do t1 e t2
-index_1 = db.insert(t1.as_dict())
-#forma 2 por empacotamento
-index_2, index3 = db.insert_multiple(
+#Inserir os dados por empacotamento
+index_1, index2 = db.insert_multiple(
     [t1.as_dict(), t2.as_dict()])
 
 #Remover dados por index
-db.remove(doc_ids=[2])
+db.remove(doc_ids=[])
 
-print(index_1)
+#atualizar qualquer dados dentro do BD, neste caso altero a tarefa task
+db.update(
+    {'task': 'Buy more coffe'},
+          #obs: como eu estou mandando no plural  doc_ids  preciso enviar mais que um indice oc_ids=1, 2, 3 no caso que irei mandar 1 só, mandei um interavel em colchetes []
+          doc_ids=[1])
+
+#Pegar os dados
+TodoQuery = Query()
+print(
+    db.search(
+        #Filtre e busque informação a partir dos nomes dos dados
+        #No .owner. pode alterar pelo nome do dados como task ou status
+        #No search() basta apenas colocar qual dados vc procura e ele retornarar o dicionario inteiro
+        #Forma 2 .owner == 'Mateus'
+        TodoQuery.owner.search("Mateus")
+    )
+)
