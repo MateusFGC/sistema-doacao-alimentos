9f6a1d9b411aea5622ab7e74331803dac59ba060
MateusFGC <165953733+MateusFGC@users.noreply.github.com>
2025-04-26 11:23:12 -0300

    Desenvolvimento da aplicação
    Criação do TinyDB
    Criação dos dados do BD
    Executar o BD
    Inserção dos dados no BD
    Limpeza dos dados repetidos
    Remoção dos dados por id

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+from pathlib import Path
 from typing import Literal
+from tinydb import TinyDB
 
 
 @dataclass
@@ -8,4 +10,27 @@ class Todo:
     owner: str
     status: Literal['done', 'todo'] = 'todo'
 
-t1 = Todo()    
+    def as_dict(self):
+        return asdict(self)
+
+#Dados do BD
+t1 = Todo('buy coffee', 'Mateus', 'todo')    
+t2 = Todo('Walk the dog', 'Mateus')    
+
+#Criação e execução do BD
+db_path = Path(__file__).parent / 'db.json'
+db = TinyDB(db_path, indent=4)
+
+#Limpa os dados repetidos
+db.truncate()
+
+#2 formas diferentes de inserir o dados do t1 e t2
+index_1 = db.insert(t1.as_dict())
+#forma 2 por empacotamento
+index_2, index3 = db.insert_multiple(
+    [t1.as_dict(), t2.as_dict()])
+
+#Remover dados por index
+db.remove(doc_ids=[2])
+
+print(index_1)
